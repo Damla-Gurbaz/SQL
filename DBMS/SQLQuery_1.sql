0334f6bda0ae3e34415cfc22fe0095b703e66ae6
Damla-Gurbaz <81505007+Damla-Gurbaz@users.noreply.github.com>
2021-08-20 19:01:36 +0200

    SQL

--- DBMS/SQLQuery_1.sql
+++ DBMS/SQLQuery_1.sql
@@ -1,27 +1,37 @@
 CREATE DATABASE Library1;
+
 Use Library1;
+
 --Create Two Schemas
 CREATE SCHEMA Book;
 ---
 CREATE SCHEMA Person;
+
+
 --create Book.Book table
 CREATE TABLE Book.Book
 (
 	[Book_ID] [int] PRIMARY KEY NOT NULL,
 	[Book_Name] [nvarchar](50) NULL
 );
+
+
 --create Book.Author table
 CREATE TABLE Book.Author
 (
 	[Author_ID] [int],
 	[Author_Name] [nvarchar](50) NULL
 	);
+
+
 --create Book.Book_Author table
 CREATE TABLE Book.Book_Author
 (
 Book_ID INT PRIMARY KEY,
 Author_ID INT NOT NULL
 );
+
+
 --create Publisher Table
 CREATE TABLE [Book].[Publisher]
 (
